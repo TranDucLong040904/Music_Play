0269266a37b4870da5dc9424f237079f1f6239b8
Tran Duc Long <tranduclong040904@gmail.com>
2025-11-19 13:10:18 +0700

    push my project 1st

---
+++ song.js
@@ -0,0 +1,29 @@
+let songs = [
+    {
+        name: "We don't talk anymore",
+        path: "music/We Dont Talk Anymore.mp3",
+        artist: "Charlie Puth",
+        image: "images/we dont talk anymore.png" // Thay \ bằng /
+    },
+
+    {
+        name: "Fire",
+        path: "music/FIRE.mp3",
+        artist: "Alan Walker",
+        image: "images/fire.png" // Thay \ bằng /
+    },
+
+    {
+        name: " Đừng làm trái tim anh đau",
+        path: "music/Dung lam trai tim anh dau.mp3",
+        artist: "Sơn Tùng MTP",
+        image: "images/dung_lam_trai_tim_anh_dau.png" // Thay \ bằng /
+    },
+
+    {
+        name: "Unstoppable",
+        path: "music/Unstoppable.mp3",
+        artist: "Sia",
+        image: "images/unstopppable.png" // Thay \ bằng /
+    },
+];
